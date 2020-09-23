75f6ab5e8e0658d4bb6094a47bc7e54530b0a8df
Bernd-L <git@bernd.pw>
2020-09-23 20:30:42 +0200

    Implemented add_event

--- src/core/projector.rs
+++ src/core/projector.rs
@@ -2,19 +2,46 @@ use crate::typings::{self, At};
 
 struct Projector<T> {
   pub event_log: Vec<typings::Event<T>>,
+  projection: Vec<T>,
 }
 
 impl<T> Projector<T> {
   pub fn new(event_log: Vec<typings::Event<T>>) -> Projector<T> {
-    Projector { event_log }
+    let ret = Projector {
+      event_log,
+      projection: vec![],
+    };
+
+    Self::parse(&ret.event_log);
+
+    ret
   }
 
-  pub fn project(&self, at: typings::At) -> Vec<T> {
+  /// Applies an event to the projection and adds
+  /// it to the event log of this instance.
+  pub fn add_event(&mut self, event: typings::Event<T>) {
+    self.event_log.push(event);
+    Self::parse_one(&mut self.projection, &event);
+  }
+
+  /// Projects an event log onto a returned list of entities
+  ///
+  /// The `at` parameter specifies until when the event log should be
+  /// projected from. Using `Latest` will return the cached projection instead.
+  pub fn project(&self, at: typings::At) -> &Vec<T> {
     match at {
-      At::Latest => (),
-      At::Date(d) => (),
+      At::Latest => &self.projection,
+      At::Date(d) => vec![],
     }
-
-    vec![]
   }
+
+  fn parse(event_log: &Vec<typings::Event<T>>) -> Vec<T> {
+    let list = vec![];
+
+    // TODO parse the event log
+
+    list
+  }
+
+  fn parse_one(list: &mut Vec<T>, event: &typings::Event<T>) {}
 }

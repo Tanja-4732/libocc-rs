e9d9469ebb75a3b46e27bcd94bf8d77e12c1a20f
Bernd-L <git@bernd.pw>
2020-09-25 16:16:31 +0200

    Begin documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+#![warn(missing_docs)]
+#![warn(missing_doc_code_examples)]
+
+//! This crate offers versioned CRUD operations for entities.
+//!
+//! Any entity is modified using events. Every event is either of type create, update or delete.
+//!
+//! You can use the [`Repository`] struct to use a nice abstraction for the underlying event logs,
+//! or use the [`Projector`] struct to access the lower-level operations of event projection.
+//!
+//! [`Repository`]: core/repository/struct.Repository.html
+//! [`Projector`]: core/projector/struct.Projector.html
+
 #[cfg(test)]
 mod test;
 
